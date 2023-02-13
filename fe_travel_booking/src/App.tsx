7eb7941be9d1230787b874c3465c32ccac793ba6
Thainguyen3003 <nguyennangthai@gmail.com>
2023-02-13 17:15:39 +0700

    update

--- fe_travel_booking/src/App.tsx
+++ fe_travel_booking/src/App.tsx
@@ -1,25 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Index = () => {
+  return <h2>Home</h2>;
+};
+
+const Product = () => {
+  return <h2>This is product</h2>;
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products/1">First Product</Link>
+            </li>
+            <li>
+              <Link to="/products/2">Second Product</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
 };
 
